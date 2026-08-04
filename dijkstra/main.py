70654f21131d0c63fc2bbcc3e586d76c36c1ae55
Nassim <achnassim34@gmail.com>
2026-08-04 14:05:43 +0200

    <feat> test pour health, shortestpath & load_datastore

--- dijkstra/main.py
+++ dijkstra/main.py
@@ -5,3 +5,7 @@ from routes.dijkstra import router as dijkstra_router
 app = FastAPI()
 
 app.include_router(router=dijkstra_router)
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
