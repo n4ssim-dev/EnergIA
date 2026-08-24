c58632145537f32857adb5af63f1573d5f106c77
Malick FALL <emfall5015@gmail.com>
2026-08-24 15:49:05 +0200

    <Feat>

--- fastapi/graph/datastore.py
+++ fastapi/graph/datastore.py
@@ -17,6 +17,7 @@ class DataStore:
         self.centrales = {}
         self.regions = {}
         self.liaisons = []
+        self.timestamps = []  
         self.graph = Graph()
 
     def load(self, path=DATA_PATH):
