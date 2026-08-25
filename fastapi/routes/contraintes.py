e32eb52e4595dee053b37a88ee88db2dcb2d13cf
Sophian Sefiani <sophiansimplon@gmail.com>
2026-08-25 11:59:28 +0200

    Calcul des bornes et des contraintes

--- fastapi/routes/contraintes.py
+++ fastapi/routes/contraintes.py
@@ -30,7 +30,31 @@ def calcul_vitesse_descente(centrale) :
     rampDown = calcul_vitesse_montee(centrale)
     return(rampDown)
 
+# ---------------------------------------------------------------------------
+# 5. Calcule contrainte en fonction de la demande
+# ---------------------------------------------------------------------------
+
 def constraint (puissance_precedente, puissance_souhaitee, centrale) :
     if puissance_souhaitee > puissance_precedente :
-        
-    else 
+       contrainte = calcul_vitesse_montee(centrale) + puissance_precedente
+       return min(contrainte,puissance_souhaitee)
+    elif puissance_souhaitee < puissance_precedente :
+        contrainte =  puissance_precedente - calcul_vitesse_descente(centrale)
+        return max (contrainte,puissance_souhaitee)
+    else :
+        return(puissance_souhaitee)
+# ---------------------------------------------------------------------------
+#6. On s'assure ici que la puissance reste entre le min et max absolus
+# ---------------------------------------------------------------------------
+
+def appliquer_bornes (puissance,centrale):
+    return max(calcul_puissance_min(centrale), min(puissance, calcul_puissance_max(centrale)))
+
+# ------------------------------------------------------------------------------------------------------------------
+#7. Calcule de la puissance reelle en utilisant les contraintes de la centrale et les bornes de puissance max et min
+# ------------------------------------------------------------------------------------------------------------------
+
+def puissance_reelle(puissance_precedente, puissance_souhaitee, centrale):
+    contrainte = constraint(puissance_precedente, puissance_souhaitee, centrale)
+    limite = appliquer_bornes(contrainte,centrale)
+    return (limite)
