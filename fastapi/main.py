1cb6b4ce9fd905ae290e76a58c56e55e1e454e6f
Nassim <achnassim34@gmail.com>
2026-08-27 09:13:16 +0200

    <feat> ingestion db fonctionnel

--- fastapi/main.py
+++ fastapi/main.py
@@ -6,6 +6,7 @@ from fastapi import Depends, FastAPI
 
 from routes.api import router as api_router
 from routes.auth import check_password
+from routes.db import router as db_router
 from routes.dijkstra import router as dijkstra_router
 
 
@@ -13,6 +14,7 @@ app = FastAPI()
 
 app.include_router(router=dijkstra_router, dependencies=[Depends(check_password)])
 app.include_router(router=api_router)
+app.include_router(router=db_router)
 
 @app.get("/health")
 def health():
