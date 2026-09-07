a3fc44d003916688a399342e5208497d1fe1ee34
Malick FALL <emfall5015@gmail.com>
2026-09-07 13:38:12 +0200

    Partie Front version1

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Chat } from './chat/chat';
 
 
 export const routes: Routes = [
-    { path: '', component: Home}
-   
-   
+
+    { path: '', component: Home },
+    { path: 'chat', component: Chat }
 ];
