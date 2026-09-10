1b95112196b346190dbcf01fdcdebcf20c28306e
Lola Erhart <erhart.lola@hotmail.fr>
2026-09-10 10:20:18 +0200

    <feat> intégration de la régréssion linéaire pour prédiction

--- ms_predictive/entrainement_ml.py
+++ ms_predictive/entrainement_ml.py
@@ -1,10 +1,15 @@
 from preparation_ml import preparer_dataframe_ml
 from analyse_donnees import charger_donnees_analytiques
 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+
+
 # ---------------------------------
 # Chargement des données
 # ---------------------------------
-
 df = charger_donnees_analytiques()
 
 df_ml = preparer_dataframe_ml(df)
@@ -12,13 +17,11 @@ df_ml = preparer_dataframe_ml(df)
 # ---------------------------------
 # Définition de la cible
 # ---------------------------------
-
 y = df_ml["consumption_mw"]
 
 # ---------------------------------
-# Définition des variables explicatives
+# Définition des features
 # ---------------------------------
-
 X = df_ml[
     [
         "id_region",
@@ -39,13 +42,18 @@ X = df_ml[
     ]
 ]
 
-print(X.head())
-print(y.head())
+# ---------------------------------
+# Séparation temporelle
+# ---------------------------------
+X_train = X[df_ml["annee"] < 2025]
+X_test = X[df_ml["annee"] == 2025]
+
+y_train = y[df_ml["annee"] < 2025]
+y_test = y[df_ml["annee"] == 2025]
 
 # ---------------------------------
 # Séparation des types de variables
 # ---------------------------------
-
 variables_categorielles = [
     "id_region",
     "saison",
@@ -67,8 +75,74 @@ variables_numeriques = [
     "part_indus_lourde",
 ]
 
-X_train = X[df_ml["annee"] < 2024]
-X_test = X[df_ml["annee"] == 2024]
+# ---------------------------------
+# Préparation de l'encodage
+# ---------------------------------
+preprocesseur = ColumnTransformer(
+    transformers=[
+        ("categoriel",OneHotEncoder(handle_unknown="ignore"),variables_categorielles)
+    ],
+    remainder="passthrough")
 
-y_train = y[df_ml["annee"] < 2024]
-y_test = y[df_ml["annee"] == 2024]
+# # ---------------------------------
+# # Apprentissage sur X_train
+# # ---------------------------------
+# X_train_encode = preprocesseur.fit_transform(X_train)
+
+# # ---------------------------------
+# # Application sur X_test
+# # ---------------------------------
+# X_test_encode = preprocesseur.transform(X_test)
+
+# # ---------------------------------
+# # Vérifications
+# # ---------------------------------
+# print("X_train avant encodage :", X_train.shape)
+# print("X_train après encodage :", X_train_encode.shape)
+
+# print("X_test avant encodage :", X_test.shape)
+# print("X_test après encodage :", X_test_encode.shape)
+
+# print("y_train :", y_train.shape)
+# print("y_test :", y_test.shape)
+
+#------------------------------------------------------------------
+# 1. Création de la régréssion linéaire
+#------------------------------------------------------------------
+X_train_prepare = preprocesseur.fit_transform(X_train)
+X_test_prepare = preprocesseur.transform(X_test)
+
+print(X_train_prepare.shape)
+print(type(X_train_prepare))
+print(X_test_prepare.shape)
+
+model_lineaire = LinearRegression()
+
+# Entrainement du modèle
+model_lineaire.fit(X_train_prepare, y_train)
+print("Entraînement terminé")
+
+# Prédiction du modèle
+prediction = model_lineaire.predict(X_test_prepare)
+
+print(prediction[:10])
+print(y_test.head(10))
+
+#------------------------------------------------------------------
+# Utilisation du MAE
+#------------------------------------------------------------------
+mae_lineaire = mean_absolute_error(y_test, prediction)
+mape_lineaire = mean_absolute_percentage_error(y_test, prediction)
+print(f"MAE régression linéaire : {mae_lineaire:.0f} MW")
+print(f"MAPE régression linéaire : {mape_lineaire * 100:.2f} %")
+
+#------------------------------------------------------------------
+# 2. Baseline naïve : préparation 2024 et 2025
+#------------------------------------------------------------------
+
+df_2024 = df_ml[df_ml["annee"] == 2024].copy()
+df_2025 = df_ml[df_ml["annee"] == 2025].copy()
+
+print("2024 :", df_2024.shape)
+print("2025 :", df_2025.shape)
+
