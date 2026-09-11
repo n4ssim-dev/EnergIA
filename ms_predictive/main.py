585e83cb2dd5a57f6f26a699d307f77a2820780c
Nassim <achnassim34@gmail.com>
2026-09-11 11:54:31 +0200

    <feat> ajout de l'interpolation pour dates manquantes meteo & ajout du boolean est_interpole

--- ms_predictive/main.py
+++ ms_predictive/main.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv
 load_dotenv()
 from fastapi import FastAPI
 from routes.ingest import router as ingest_router, get_target_connection
+from routes.diagnostics import router as diagnostics_router
 
 app = FastAPI()
 app.include_router(router=ingest_router)
+app.include_router(router=diagnostics_router)
 
 @app.get("/health")
 def health():
