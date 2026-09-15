fc1a5af997077266e38b3eb33f982b41179b267b
Nassim <achnassim34@gmail.com>
2026-09-15 09:36:54 +0200

    <fix> db.py, docker compose

--- ms_predictive/utils/db.py
+++ ms_predictive/utils/db.py
@@ -1,13 +1,15 @@
+import os
+
 import psycopg
 
 
 def connect_bdd():
     connexion = psycopg.connect(
-        host="localhost",
-        port=5435,
-        user="energia",
-        password="energia",
-        dbname="energia_predictive",
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        port=os.getenv("POSTGRES_PORT", "5435"),
+        user=os.getenv("POSTGRES_USER", "energia"),
+        password=os.getenv("POSTGRES_PASSWORD", "energia"),
+        dbname=os.getenv("POSTGRES_DB", "energia_predictive"),
     )
 
     return connexion
